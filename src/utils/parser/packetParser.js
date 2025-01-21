214a9cfbf61622587321d181975475b8520208f3
foreiner <74106693+foreiner@users.noreply.github.com>
2025-01-21 22:59:13 +0900

    update packetParser.js
    클라이언트로 전달할때 사용할 인코딩 작성중

--- src/utils/parser/packetParser.js
+++ src/utils/parser/packetParser.js
@@ -2,6 +2,7 @@ import { getProtoMessages } from '../../init/loadProtos.js';
 import { getProtoTypeNameByHandlerId } from '../../handlers/index.js';
 import CustomError from '../error/customError.js';
 import { ErrorCodes } from '../error/errorCodes.js';
+
 export const packetParser = (handlerId, rawPayload) => {
   const protoMessages = getProtoMessages();
 
@@ -32,4 +33,20 @@ export const packetParser = (handlerId, rawPayload) => {
   }
 
   return payload;
+};
+// 클라이언트로 전달할 패킷으로 인코딩
+export const payloadParser = (packetType, rawPayload) => {
+    // 패킷 길이 정보를 포함한 버퍼 생성
+    const packetLength = Buffer.alloc(config.packet.totalLength);
+    packetLength.writeUInt32BE(
+      message.length + config.packet.totalLength + config.packet.typeLength,
+      0,
+    );
+  
+    // 패킷 타입 정보를 포함한 버퍼 생성
+    const packetType = Buffer.alloc(config.packet.typeLength);
+    packetType.writeUInt8(type, 0);
+  
+    // 길이 정보와 메시지를 함께 전송
+    return Buffer.concat([packetLength, packetType, message]);
 };
