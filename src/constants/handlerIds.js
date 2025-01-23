a271d38c6bf24233f24be599621f28989d4bf7d7
Jaeyoung <jyko1101@gmail.com>
2025-01-23 14:47:56 +0900

    fix: handler id fixed

--- src/constants/handlerIds.js
+++ src/constants/handlerIds.js
@@ -12,7 +12,8 @@ export const RESPONSE_SUCCESS_CODE = 0;
 export const HANDLER_IDS = {
   INITIAL: 0,
   REGISTER: 1,
-  LOGIN: 2,
+  // LOGIN: 2,
+  LOGIN: 3,
   CREATE_GAME: 4,
   MATCH_MAKE: 5,
   JOIN_GAME: 16,
