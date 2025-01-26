4d0e070f1949201a6e71144a804179b615dcc0e0
foreiner <74106693+foreiner@users.noreply.github.com>
2025-01-26 16:27:09 +0900

    fix
    없어진 파일 수정...

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -13,6 +13,10 @@ import loginHandler from './user/login.handler.js';
 import purchaseTowerHandler from './tower/purchaseTower.handler.js';
 import matchResponseHandler from './game/matchResponse.handler.js';
 import monsterAttackBaseRequestHandler from './base/monsterAttackBaseRequest.handler.js';
+import updategameOverNotificationHandler from './game/gameOverNotification.handler.js'
+import spawnMonsterReqHandler from './game/gameOverNotification.handler.js'
+import updateBaseHpNotificationHandler from './base/updateBaseHpNotification.handlers.js'
+import gameEndRequestHandler from './game/gameEndRequest.handler.js'
 const handlers = {
   [HANDLER_IDS.INITIAL]: {
     handler: initialHandler,
