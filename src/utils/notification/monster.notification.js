bedf1e21f88cd8af24977a083334349515d25936
Youmin Lee <youmin0107@naver.com>
2025-01-26 14:59:24 +0900

    feat:monsterDeathNotificationHandler

--- src/utils/notification/monster.notification.js
+++ src/utils/notification/monster.notification.js
@@ -5,7 +5,6 @@ import { config } from '../../config/config.js';
 import { payloadParser } from '../parser/packetParser.js';
 import { testLog } from '../testLogger.js';
 
-
 export const makeNotification = (message, type) => {
   // 패킷 길이 정보를 포함한 버퍼 생성
   const packetLength = Buffer.alloc(config.packet.totalLength);
@@ -22,44 +21,73 @@ export const makeNotification = (message, type) => {
   return Buffer.concat([packetLength, packetType, message]);
 };
 
-export const createS2CSpawnMonsterResponse = (monsterId,monsterNumber,user) => {
+export const createS2CSpawnMonsterResponse = (monsterId, monsterNumber, user) => {
   const protoMessages = getProtoMessages();
   const dataType = protoMessages.test.GamePacket;
 
-  if(!dataType) {
+  if (!dataType) {
     throw new Error('dataType 메세지 정의 x');
   }
 
-  testLog(0,`dataType: ${dataType}`,'green');
+  testLog(0, `dataType: ${dataType}`, 'green');
 
   const payload = {
     spawnMonsterResponse: {
       monsterId,
       monsterNumber,
-    }
+    },
   };
 
-  testLog(0,`payload:${JSON.stringify(payload)} `,'yellow');
+  testLog(0, `payload:${JSON.stringify(payload)} `, 'yellow');
 
   const message = dataType.create(payload);
   const monsterSpawnPacket = dataType.encode(message).finish();
-  testLog(0,`message: ${JSON.stringify(message)}`,'red');
-  testLog(0,`monsterSpawnPacket: ${monsterSpawnPacket.toString('hex')}`,'blue');
+  testLog(0, `message: ${JSON.stringify(message)}`, 'red');
+  testLog(0, `monsterSpawnPacket: ${monsterSpawnPacket.toString('hex')}`, 'blue');
 
   // return makeNotification(monsterSpawnPacket,PACKET_TYPE.SPAWN_MONSTER_RESPONSE);
- return payloadParser(PACKET_TYPE.SPAWN_MONSTER_RESPONSE,user,monsterSpawnPacket);
-
+  return payloadParser(PACKET_TYPE.SPAWN_MONSTER_RESPONSE, user, monsterSpawnPacket);
 };
 
-export const createS2CSpawnEnemyMonsterNotification = (monsterId,monsterNum) => {
+export const createS2CSpawnEnemyMonsterNotification = (monsterId, monsterNumber, user) => {
   const protoMessages = getProtoMessages();
-  const dataType = protoMessages.test.S2CSpawnEnemyMonsterNotification;
+  const dataType = protoMessages.test.GamePacket;
 
-  const payload = {monsterId,monsterNum};
+  if (!dataType) {
+    throw new Error('dataType 메세지 정의 x');
+  }
+
+  const payload = {
+    spawnMonsterResponse: {
+      monsterId,
+      monsterNumber,
+    },
+  };
+
   const message = dataType.create(payload);
   const monsterSpawnPacket = dataType.encode(message).finish();
-  return makeNotification(monsterSpawnPacket,PACKET_TYPE.SPAWN_ENEMY_MONSTER_NOTIFICATION);
+
+  // return makeNotification(monsterSpawnPacket,PACKET_TYPE.SPAWN_MONSTER_RESPONSE);
+  return payloadParser(PACKET_TYPE.SPAWN_ENEMY_MONSTER_NOTIFICATION, user, monsterSpawnPacket);
 };
 
+export const createS2CEnemyMonsterDeathNotification = (monsterId, user) => {
+  const protoMessages = getProtoMessages();
+  const dataType = protoMessages.test.GamePacket;
 
+  if (!dataType) {
+    throw new Error('dataType 메세지 정의 x');
+  }
 
+  const payload = {
+    enemyMonsterDeathNotification: {
+      monsterId,
+    },
+  };
+
+  const message = dataType.create(payload);
+  const monsterDeathPacket = dataType.encode(message).finish();
+
+  // return makeNotification(monsterSpawnPacket,PACKET_TYPE.SPAWN_MONSTER_RESPONSE);
+  return payloadParser(PACKET_TYPE.ENEMY_MONSTER_DEATH_NOTIFICATION, user, monsterDeathPacket);
+};
