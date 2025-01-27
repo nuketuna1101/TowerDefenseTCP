99441f9fadaa7a0cdcc5b96c44b0d044a942b402
ssy1248 <ssy1248@gmail.com>
2025-01-27 16:47:00 +0900

    onEnd Modify

--- src/events/onEnd.js
+++ src/events/onEnd.js
@@ -3,5 +3,13 @@ import { removeUser } from '../session/user.session.js';
 
 export const onEnd = (socket) => () => {
   console.log('클라이언트 연결이 종료되었습니다.');
+  const user = getUserBySocket(socket);
+  const gameSession = getGameByUser(user);
+  if (gameSession) {
+    const handler = getHandlerById(18);
+    handler({
+      user: user,
+    });
+  }
   removeUser(socket);
 };
