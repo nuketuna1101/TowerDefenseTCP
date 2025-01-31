c520569c175dba064a77d45efce0523a1948afa8
Jaeyoung <jyko1101@gmail.com>
2025-01-31 13:22:39 +0900

    fix: tower attack w. dead monster

--- src/handlers/tower/attackRequest.handler.js
+++ src/handlers/tower/attackRequest.handler.js
@@ -8,10 +8,12 @@
 //====================================================================================================================
 
 import TowerManager from "../../classes/managers/tower.manager.js";
+import { getGameByUserId } from "../../session/game.session.js";
 import { getUserById } from "../../session/user.session.js";
 import CustomError from "../../utils/error/customError.js";
 import { ErrorCodes } from "../../utils/error/errorCodes.js";
 import { handleError } from "../../utils/error/errorHandler.js";
+import { enemyTowerAttackNotification } from "../../utils/notification/tower.notification.js";
 import { testLog } from "../../utils/testLogger.js";
 import { getMonsterById } from './../../session/monster.session.js';
 
@@ -32,17 +34,35 @@ const attackRequestHandler = ({ socket, userId, payload }) => {
             throw new CustomError(ErrorCodes.CANNOT_FIND, "Cannot find target Tower");
         if (!tower.isOwnedBy(userId))
             throw new CustomError(ErrorCodes.NOT_AUTHORIZED_ACCESS, "tower is NOT owned by user");
-    
+
         // monster 존재여부 << monster session에서 찾아오기
 
 
         // Monster class instance 를 가져와야 하는데
         const monster = user.getMonsterById(monsterId);//getMonsterByMonsterId(monsterId);//
-        if (!monster)
-            throw new CustomError(ErrorCodes.CANNOT_FIND, "Cannot find target monster");
+        // if (!monster)
+        //     throw new CustomError(ErrorCodes.CANNOT_FIND, "Cannot find target monster");
 
         // 타워의 몬스터에 대한 공격 처리
+        // tower.attack(monster);
+
+
+        // 유저가 자신이 속한 게임 세션 내의 유저들에게 notify
+
+        const game = getGameByUserId(userId);
+        const users = game.getUsers();
+        users.forEach((user) => {
+            // 자기 자신에게는 보내지 않음
+            if (user.id == userId) return;
+            //
+            // if (!monster) return;
+            const enemyTowerAttackPacket = enemyTowerAttackNotification(towerId, monsterId, user);
+            user.socket.write(enemyTowerAttackPacket);
+        });
+
+
         tower.attack(monster);
+
     } catch (error) {
         handleError(socket, error);
     }
